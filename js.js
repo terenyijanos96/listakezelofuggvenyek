d05ecd862f55122969549b50970c6ecfc359b7d7
Gergely Patricia Róza (SZF_E_2022_2024) <gergely.patricia.roza@diak.szamalk-szalezi.hu>
2023-03-18 08:52:53 +0100

    first commit

---
+++ js.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", init);
+const KUTYALISTA=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
+
+function init() {
+    const ARTICLE = document.getElementsByName("article");
+    const GOMB = document.createElement('button');
+    GOMB.innerText = ('Kutya');
+    document.body.appendChild(GOMB);
+};
+
+function gombNyomas (){
+    
+}
+
