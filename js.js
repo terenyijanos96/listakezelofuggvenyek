6496636bd296c07afa252a6db14927ee1be59c3a
PatrsiaRose <cseresznyevirag4@gmail.com>
2023-03-25 00:56:37 +0100

    radio betétele

--- js.js
+++ js.js
@@ -1,25 +1,63 @@
 window.addEventListener("load", init);
 const KUTYALISTA = [
-  { nev: "Bodri", kor: 12, fajta: "foxterrier", lab: 4 },
-  { nev: "Kutya", kor: 8, fajta: "kuvasz", lab: 4 },
-  { nev: "Vak", kor: 14, fajta: "puli", lab: 4 },
-  { nev: "Sovika", kor: 11, fajta: "agár", lab: 4 },
-  { nev: "Lédi", kor: 12, fajta: "újfullandi", lab: 4 },
-  { nev: "Lessie", kor: 9, fajta: "keverék", lab: 4 },
+  {
+    nev: "Bodri",
+    kor: 12,
+    fajta: "foxterrier",
+    lab: 4,
+    nem: "kan",
+    marmagasság: 35,
+  },
+  {
+    nev: "Kutya",
+    kor: 8,
+    fajta: "kuvasz",
+    lab: 4,
+    nem: "szuka",
+    marmagasság: 70,
+  },
+  { nev: "Vak", kor: 14, fajta: "puli", lab: 4, nem: "kan", marmagasság: 40 },
+  {
+    nev: "Sovika",
+    kor: 11,
+    fajta: "agár",
+    lab: 4,
+    nem: "kan",
+    marmagasság: 65,
+  },
+  {
+    nev: "Lédi",
+    kor: 12,
+    fajta: "újfundlandi",
+    lab: 4,
+    nem: "kan",
+    marmagasság: 70,
+  },
+  {
+    nev: "Lessie",
+    kor: 9,
+    fajta: "keverék",
+    lab: 4,
+    nem: "szuka",
+    marmagasság: 50,
+  },
 ];
 let ARTICLE;
+let kartyak;
+let tablazat;
 /* const KUTYANEVEK=["Bodri","Kutya","Vak","Sovika","Lédi","Lessie"]
 const KUTYAFAJTAK = ["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
 const KUTYAKOROK = [12, 8, 14, 11, 12, 9] */
 function init() {
   ARTICLE = document.querySelector("article");
+  kartyak = document.querySelector("section.kartyak");
+  tablazat = document.querySelector("section.tablazat");
   /* const GOMB = document.createElement('button');
     GOMB.innerText = ('Kutya');
     ARTICLE.appendChild(GOMB); */
   /* GOMB.addEventListener("click", function(){gombNyomas(ARTICLE)}) */
-  //let txt = osszeallit();
-  let txt2 = osszeallit2();
-  //ARTICLE.innerHTML=txt;
+  osszeallit();
+  osszeallit2();
   torlesGomb();
   const SUBMIT = document.querySelector("aside button");
   SUBMIT.addEventListener("click", ujKutya);
@@ -44,26 +82,25 @@ function init() {
     return txt;
 } */
 
-/* function osszeallit2() {
-  let txt2 = "";
-  txt2 += `<div class = "container">`;
+function osszeallit() {
+  let txt = "";
+  txt += `<div class = "container">`;
   for (let index = 0; index < KUTYALISTA.length; index++) {
-    /* for (const key in KUTYALISTA) {
-            KUTYALISTA[key]
-        }
-        for (const kutya of KUTYALISTA) {
-            
-        } */
-/*    txt2 += `<div class= "kartya">`;
-    for (const kulcs in KUTYALISTA[index]) {
-      txt2 += `<p>${kulcs}: ${KUTYALISTA[index][kulcs]}</p>`;
+    for (const key in KUTYALISTA) {
+      KUTYALISTA[key];
     }
-    txt2 += `</div>`;
+    for (const kutya of KUTYALISTA) {
+    }
+    txt += `<div class= "kartya">`;
+    txt += `<h3>Kutya adatai</h3>`;
+    for (const kulcs in KUTYALISTA[index]) {
+      txt += `<p>${kulcs}: ${KUTYALISTA[index][kulcs]}</p>`;
+    }
+    txt += `</div>`;
   }
-  txt2 += `</div>`;
-  console.log(txt2);
-  return txt2;
-}  */
+  txt += `</div>`;
+  kartyak.innerHTML = txt;
+}
 
 function osszeallit2() {
   let txt2 = "";
@@ -84,8 +121,7 @@ function osszeallit2() {
   }
   txt2 += `</table>`;
   txt2 += `</div>`;
-  console.log(txt2);
-  ARTICLE.innerHTML = txt2;
+  tablazat.innerHTML = txt2;
 }
 
 function torlesGomb() {
@@ -105,21 +141,30 @@ function torlesGomb() {
 
 function torlesFunkcio(index) {
   KUTYALISTA.splice(index, 1);
-  console.log(KUTYALISTA);
   osszeallit2();
   torlesGomb();
 }
 
 function ujKutya() {
   let Kutya = {};
+  let szuka = document.querySelector("#szuka");
+  let kan = document.querySelector("#kan");
   const ADAT = document.querySelectorAll("input");
   console.log("Vauka");
   index = 0;
   for (const kulcs in KUTYALISTA[index]) {
-    Kutya[kulcs] = `${ADAT[index].value}`;
+    if (ADAT[index].id == "szuka" && (szuka.checked = true)) {
+      console.log("szuka");
+      Kutya[kulcs] = "szuka";
+      index++;
+    } else if (ADAT[index].id == "kan" && (kan.checked = true)) {
+      console.log("kan");
+      Kutya[kulcs] = "kan";
+    } else {
+      Kutya[kulcs] = `${ADAT[index].value}`;
+    }
     index++;
   }
-
   KUTYALISTA.push(Kutya);
   console.log(KUTYALISTA);
   osszeallit2();
